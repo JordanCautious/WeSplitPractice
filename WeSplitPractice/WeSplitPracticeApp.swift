0e2aa6c1c6495aaa4cb4839436575454a7166f81
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-08-25 17:45:03 -0400

    Initial Commit

---
+++ WeSplitPractice/WeSplitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitPracticeApp.swift
+//  WeSplitPractice
+//
+//  Created by Jordan Haynes on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
